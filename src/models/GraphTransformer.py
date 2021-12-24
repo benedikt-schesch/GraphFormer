9cc92e72a534dfaddd2d93c25d50f0621593dbeb
Benedikt <scheschb@student.ethz.ch>
2021-12-24 10:08:31 +0100

    Added different models and baselines

--- src/models/GraphTransformer.py
+++ src/models/GraphTransformer.py
@@ -100,7 +100,7 @@ class GraphTransformer(nn.Module):
             clusters[cluster[-1]] = cluster_id
             cluster_is_full = False
             while not cluster_is_full:
-                if len(neighbs) == 0:
+                if len(neighbs) == 0: #We have no neighbors so we add some random unvisited node
                     if len(unvisited_nodes) == 0:
                         cluster_is_full = True
                         break
@@ -112,7 +112,7 @@ class GraphTransformer(nn.Module):
                     if len(cluster) == self.target_cluster_size:
                         cluster_is_full = True
                         break
-                while len(neighbs) > 0:
+                while len(neighbs) > 0: #We do have neighbors so we add one randomly
                     cluster.append(random.sample(neighbs,1)[0])
                     neighbs.remove(cluster[-1])
                     unvisited_nodes.remove(cluster[-1])
@@ -127,11 +127,13 @@ class GraphTransformer(nn.Module):
 
     def forward(self, g ,adj):
         if g.x == None: #In case we don't have any Node features we learn an embedding
-            g.x = self.node_embedding_layer(torch.tensor([0]))[0].repeat(g.num_nodes,1)
+            x = self.node_embedding_layer(torch.tensor([0]))[0].repeat(g.num_nodes,1)
+        else:
+            x = g.x
         clusters, num_clusters = self.generate_clusters(g.edge_index,g.num_nodes,adj)
-        x = self.input_layer(g.x)
+        x = self.input_layer(x)
         x = self.conv1(x,g.edge_index)
-        x = self.conv2(x,g.edge_index)
+        #x = self.conv2(x,g.edge_index)
         cluster_embeddings = []
         for i in range(num_clusters-1):
             cluster_elements = np.nonzero(clusters==i)[0]
@@ -141,6 +143,143 @@ class GraphTransformer(nn.Module):
         cluster_embeddings.append(self.embeddings_layer(torch.cat((x[cluster_elements].view(-1),
         torch.zeros(self.embedding_dim*self.target_cluster_size-x[cluster_elements].view(-1).shape[0])))))
 
+        cluster_embeddings = [self.class_token(torch.zeros(1).type(torch.LongTensor)).view(-1)] + cluster_embeddings
+        cluster_embeddings = torch.stack(cluster_embeddings)
+        result = self.transformer(cluster_embeddings.view((1,cluster_embeddings.shape[0],cluster_embeddings.shape[1])))
+        return self.to_class(result[0][0].view(1,-1))
+
+
+
+class ConvAggregation(nn.Module):
+    def __init__(self,embedding_dim,num_features,num_clases,device):
+        super().__init__()
+        self.to_class = nn.LazyLinear(num_clases)
+        self.embedding_dim = embedding_dim
+        self.device = device
+        self.embeddings_layer = nn.LazyLinear(embedding_dim)
+        self.input_layer = nn.LazyLinear(embedding_dim)
+        self.conv1 = GATConv(embedding_dim,embedding_dim)
+        self.conv2 = GATConv(embedding_dim,embedding_dim)
+        self.conv3 = GATConv(embedding_dim,embedding_dim)
+        self.conv4 = GATConv(embedding_dim,embedding_dim)
+        self.node_embedding_layer = nn.Embedding(1,embedding_dim)
+
+    def forward(self, g ,adj):
+        if g.x == None: #In case we don't have any Node features we learn an embedding
+            x = self.node_embedding_layer(torch.tensor([0]))[0].repeat(g.num_nodes,1)
+        else:
+            x = g.x
+        x = self.input_layer(x)
+        x = self.conv1(x,g.edge_index)
+        x = self.conv2(x,g.edge_index)
+        x = self.conv3(x,g.edge_index)
+        x = self.conv4(x,g.edge_index)
+
+        x = torch.mean(x,0)
+        return self.to_class(x).view(1,-1)
+    
+
+
+class FullGraphTransformer(nn.Module):
+    def __init__(self,embedding_dim,num_features,num_clases,device):
+        super().__init__()
+        self.to_class = nn.LazyLinear(num_clases)
+        self.embedding_dim = embedding_dim
+        self.device = device
+        self.target_cluster_size = 32
+        self.embeddings_layer = nn.LazyLinear(embedding_dim)
+        self.transformer = Transformer(depth=8,heads=8,dim=embedding_dim)
+        self.class_token = nn.Embedding(1, embedding_dim)
+        self.input_layer = nn.LazyLinear(embedding_dim)
+        self.conv1 = GATConv(embedding_dim,embedding_dim)
+        self.conv2 = GATConv(embedding_dim,embedding_dim)
+        self.node_embedding_layer = nn.Embedding(1,embedding_dim)
+
+    def forward(self, g ,adj):
+        if g.x == None: #In case we don't have any Node features we learn an embedding
+            x = self.node_embedding_layer(torch.tensor([0]))[0].repeat(g.num_nodes,1)
+        else:
+            x = g.x
+        #clusters, num_clusters = self.generate_clusters(g.edge_index,g.num_nodes,adj)
+        x = self.input_layer(x)
+        #x = self.conv1(x,g.edge_index)
+        #x = self.conv2(x,g.edge_index)
+        #cluster_embeddings = []
+        #for i in range(num_clusters-1):
+        #    cluster_elements = np.nonzero(clusters==i)[0]
+        #    cluster_embeddings.append(self.embeddings_layer(x[cluster_elements].view(-1)))
+        
+        #cluster_elements = np.nonzero(clusters==(num_clusters-1))[0]
+        
+        x = torch.cat((self.class_token(torch.zeros(1).type(torch.LongTensor)),x))
+        result = self.transformer(x.view((1,x.shape[0],x.shape[1])))
+        return self.to_class(result[0][0].view(1,-1))
+
+
+
+class RandomGraphTransformer(nn.Module):
+    def __init__(self,embedding_dim,num_features,num_clases,device):
+        super().__init__()
+        self.to_class = nn.LazyLinear(num_clases)
+        self.embedding_dim = embedding_dim
+        self.device = device
+        self.target_cluster_size = 32
+        self.embeddings_layer = nn.LazyLinear(embedding_dim)
+        self.transformer = Transformer(depth=8,heads=8,dim=embedding_dim)
+        self.class_token = nn.Embedding(1, embedding_dim)
+        self.input_layer = nn.LazyLinear(embedding_dim)
+        self.conv1 = GATConv(embedding_dim,embedding_dim)
+        self.conv2 = GATConv(embedding_dim,embedding_dim)
+        self.node_embedding_layer = nn.Embedding(1,embedding_dim)
+    
+    def generate_clusters(self,edge_index, num_nodes, adj):
+        #edges = edge_index.transpose(1,0).tolist()
+        #adj = {k: [v[1] for v in g] for k, g in groupby(sorted(edges), lambda e: e[0])}
+        clusters = np.full(num_nodes,0)
+        unvisited_nodes = set({i for i in range(num_nodes)})
+        cluster_id = 0
+        while len(unvisited_nodes) != 0:
+            neighbs = set({})
+            cluster = [random.sample(unvisited_nodes,1)[0]]
+            unvisited_nodes.remove(cluster[-1])
+            neighbs.update(adj[cluster[-1]])
+            neighbs = neighbs.intersection(unvisited_nodes)
+            clusters[cluster[-1]] = cluster_id
+            cluster_is_full = False
+            while not cluster_is_full:
+                if len(unvisited_nodes) == 0:
+                    cluster_is_full = True
+                    break
+                cluster.append(random.sample(unvisited_nodes,1)[0])
+                unvisited_nodes.remove(cluster[-1])
+                neighbs.update(adj[cluster[-1]])
+                neighbs = neighbs.intersection(unvisited_nodes)
+                clusters[cluster[-1]] = cluster_id
+                if len(cluster) == self.target_cluster_size:
+                    cluster_is_full = True
+                    break
+            cluster_id+=1
+        return clusters, cluster_id
+
+    def forward(self, g ,adj):
+        if g.x == None: #In case we don't have any Node features we learn an embedding
+            x = self.node_embedding_layer(torch.tensor([0]))[0].repeat(g.num_nodes,1)
+        else:
+            x = g.x
+        clusters, num_clusters = self.generate_clusters(g.edge_index,g.num_nodes,adj)
+        x = self.input_layer(x)
+        x = self.conv1(x,g.edge_index)
+        #x = self.conv2(x,g.edge_index)
+        cluster_embeddings = []
+        for i in range(num_clusters-1):
+            cluster_elements = np.nonzero(clusters==i)[0]
+            cluster_embeddings.append(self.embeddings_layer(x[cluster_elements].view(-1)))
+        
+        cluster_elements = np.nonzero(clusters==(num_clusters-1))[0]
+        cluster_embeddings.append(self.embeddings_layer(torch.cat((x[cluster_elements].view(-1),
+        torch.zeros(self.embedding_dim*self.target_cluster_size-x[cluster_elements].view(-1).shape[0])))))
+
+        cluster_embeddings = [self.class_token(torch.zeros(1).type(torch.LongTensor)).view(-1)] + cluster_embeddings
         cluster_embeddings = torch.stack(cluster_embeddings)
         result = self.transformer(cluster_embeddings.view((1,cluster_embeddings.shape[0],cluster_embeddings.shape[1])))
         return self.to_class(result[0][0].view(1,-1))
--- train_tu.py
+++ train_tu.py
@@ -21,10 +21,11 @@ from ogb.graphproppred import DglGraphPropPredDataset, collate_dgl
 import matplotlib.pyplot as plt
 from ogb.graphproppred import Evaluator
 from torchsampler import ImbalancedDatasetSampler
-from torch_geometric.loader import DataLoader
+from torch.utils.data import DataLoader
 
 
 def evaluate(model,dataset2,loader,loss,device):
+    model.eval()
     num_correct = 0
     total_loss = 0
     num_items = 0
@@ -68,81 +69,91 @@ def main(args):
         adj = {k: [v[1] for v in g] for k, g in groupby(sorted(edges), lambda e: e[0])}
         return x, adj
     
-    train_idx, test_idx = train_test_split(torch.arange(len(dataset)),test_size=0.25)
     dataset2 = list(map(f,dataset))
-    train_loader = DataLoader(train_idx, batch_size=args.batch_size,shuffle=True)
-    test_loader = DataLoader(test_idx,batch_size=args.batch_size)
+    skf = StratifiedKFold(n_splits=10, shuffle = True, random_state = 42)
+    test_accs = []
+    for train_idx,test_idx in skf.split(range(len(dataset)),[i.y.item() for i in dataset]):
+        test_accs.append([])
+        #print(split)
+        train_loader = DataLoader(list(train_idx), batch_size=args.batch_size,shuffle=True)
+        test_loader = DataLoader(list(test_idx),batch_size=args.batch_size)
     
-    model = GraphTransformer(args.embedding_dim,num_features=dataset.num_features,num_clases=dataset.num_classes,device=device)
-    model.to(device)
-    optimizer = get_optimizer(args,model)
-    loss_func = get_loss(args)
-    scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer,step_size=20)
+        model = RandomGraphTransformer(args.embedding_dim,num_features=dataset.num_features,num_clases=dataset.num_classes,device=device)
+        model.to(device)
+        optimizer = get_optimizer(args,model)
+        loss_func = get_loss(args)
+        scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer,step_size=20)
 
-    train_infos = pd.DataFrame(columns=['Epoch','Training accuracy','Testing Accuracy','Training loss','Testing loss'])
+        train_infos = pd.DataFrame(columns=['Epoch','Training accuracy','Testing Accuracy','Training loss','Testing loss'])
+        for epoch in range(args.epochs):
+            with tqdm(train_loader, unit="batch",disable=(args.verbose==0)) as tepoch:
+                model.train()
+                total_correct = 0
+                total_loss = 0
+                num_items = 0
+                for idx in tepoch:
+                    tepoch.set_description(f"Epoch {epoch}")
+                    x, adj = dataset2[idx]
+                    x = x.to(device)
+                    label = x.y
+                    
+                    optimizer.zero_grad()
+                    y = model(x,adj)
+                    #make_dot(y[0], params=dict(list(model.named_parameters()))).save("rnn_torchviz")
 
-    for epoch in range(args.epochs):
-        with tqdm(train_loader, unit="batch",disable=(args.verbose==0)) as tepoch:
-            total_correct = 0
-            total_loss = 0
-            num_items = 0
-            for idx in tepoch:
-                tepoch.set_description(f"Epoch {epoch}")
-                x, adj = dataset2[idx]
-                x = x.to(device)
-                label = x.y
-                
-                optimizer.zero_grad()
-                y = model(x,adj)
-                #make_dot(y[0], params=dict(list(model.named_parameters()))).save("rnn_torchviz")
+                    num_items += len(y)
+                    predictions = y.argmax(dim=1, keepdim=True).squeeze()
+                    loss = loss_func(y, label.view(-1))
+                    loss.backward()
+                    correct = (predictions == label.view(-1)).sum().item()
+                    #torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
 
-                num_items += len(y)
-                predictions = y.argmax(dim=1, keepdim=True).squeeze()
-                loss = loss_func(y, label.view(-1))
-                correct = (predictions == label.view(-1)).sum().item()
-                #torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
+                    optimizer.step()
+                    total_correct += correct
+                    total_loss += loss.item()*args.batch_size
+                    tepoch.set_postfix(loss=total_loss/num_items, accuracy=total_correct/num_items)
+                scheduler1.step()
+                train_accuracy = total_correct/num_items
+                train_loss = total_loss/num_items
+                test_accuracy, test_loss = evaluate(model,dataset2,test_loader,loss_func,device)
+                test_accs[-1].append(test_accuracy)
+                train_infos = train_infos.append({"Epoch":epoch,
+                                "Training accuracy":train_accuracy,
+                                "Testing accuracy": test_accuracy,
+                                "Training loss":train_loss,
+                                "Testing loss":test_loss},ignore_index=True)
 
-                loss.backward()
-                optimizer.step()
-                total_correct += correct
-                total_loss += loss.item()*args.batch_size
-                tepoch.set_postfix(loss=total_loss/num_items, accuracy=total_correct/num_items)
-            scheduler1.step()
-            train_accuracy = total_correct/num_items
-            train_loss = total_loss/num_items
-            test_accuracy, test_loss = evaluate(model,dataset2,test_loader,loss_func,device)
+                #Create accuracy and loss plots
+                ax = train_infos[['Training accuracy','Testing accuracy']].plot(xlim=[0,args.epochs-1],ylim=[0,1])
+                plt.xlabel('Epoch')
+                plt.ylabel('Accuracy')
+                ax.get_figure().savefig(out_folder+"Accuracy.png")
+                plt.close()
 
-            train_infos = train_infos.append({"Epoch":epoch,
-                            "Training accuracy":train_accuracy,
-                            "Testing accuracy": test_accuracy,
-                            "Training loss":train_loss,
-                            "Testing loss":test_loss},ignore_index=True)
+                ax = train_infos[['Training loss','Testing loss']].plot(xlim=[0,args.epochs-1])
+                plt.xlabel('Epoch')
+                plt.ylabel('Loss')
+                ax.get_figure().savefig(out_folder+"Loss.png")
+                plt.close()
 
-            #Create accuracy and loss plots
-            ax = train_infos[['Training accuracy','Testing accuracy']].plot(xlim=[0,args.epochs-1],ylim=[0,1])
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            ax.get_figure().savefig(out_folder+"Accuracy.png")
-
-            ax = train_infos[['Training loss','Testing loss']].plot(xlim=[0,args.epochs-1])
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            ax.get_figure().savefig(out_folder+"Loss.png")
-
-            train_infos.to_csv(out_folder+"train_infos.csv")
-    test_accuracy, test_loss = evaluate(model,dataset2,test_loader,loss_func,device)
+                train_infos.to_csv(out_folder+"train_infos.csv")
+        #test_accuracy, test_loss = evaluate(model,dataset2,test_loader,loss_func,device)
+    test_accs = np.array(test_accs)
+    max_arg = np.argmax(np.mean(test_accs,0))
+    print("Max Acuracy: ",np.mean(test_accs,0)[max_arg])
+    print("Std: ",np.std(test_accs[:,max_arg]))
     return train_accuracy, test_accuracy
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', type=int, default=1, help="Options are: 0, 1")
-    parser.add_argument('--dataset', type=str, default='DD', help="Options are: DD,FIRSTMM_DB,REDDIT-BINARY")
+    parser.add_argument('--dataset', type=str, default='REDDIT-BINARY', help="Options are: DD,FIRSTMM_DB,REDDIT-BINARY")
     parser.add_argument('--optimizer', type=str, default='adam', help="Options are: adam, sgd")
     parser.add_argument('--loss', type=str, default='crossentropyloss', help="Options are: crossentropyloss")
     parser.add_argument('--lr', type=float, default=0.0003)
     parser.add_argument('--weight_decay', type=float, default=1e-7)
-    parser.add_argument('--epochs', type=int, default=70)
+    parser.add_argument('--epochs', type=int, default=40)
     parser.add_argument('--embedding_dim', type=int, default=64)
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--output_folder', type=str, default="figs/")
